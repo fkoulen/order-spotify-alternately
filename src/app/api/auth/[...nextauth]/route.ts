c745cef645613a2eebfa734e5ab1d41e39553e75
F.S. Koulen <feryllia.koulen@hva.nl>
2023-07-14 01:53:47 +0200

    Add secret environment variable

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,16 +3,14 @@ import SpotifyProvider from "next-auth/providers/spotify"
 import querystring from "querystring"
 import { env } from "@/helpers/env.helper"
 
-export const client_id = env("NEXT_PUBLIC_SPOTIFY_CLIENT_ID") // Your client id
-export const client_secret = env("NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET") // Your secret
-
 const scope = "user-read-private user-read-email"
 
 const handler = NextAuth({
+  secret: env("SECRET"),
   providers: [
     SpotifyProvider({
-      clientId: client_id,
-      clientSecret: client_secret!,
+      clientId: env("SPOTIFY_CLIENT_ID"),
+      clientSecret: env("SPOTIFY_CLIENT_SECRET")!,
       authorization: `https://accounts.spotify.com/authorize?${querystring.stringify({ scope: scope })}`,
     }),
   ],
