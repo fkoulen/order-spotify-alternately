9c799b373c004cca6aa3b9b0abc2c18a6a4d10a8
F.S. Koulen <feryllia.koulen@hva.nl>
2023-07-14 01:29:50 +0200

    Install Flowbite and add styling to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,58 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react"
+import { Avatar, Button, CustomFlowbiteTheme, Flowbite, Spinner } from "flowbite-react"
 
 export default function Home() {
-  const { data } = useSession()
+  const { data: session, status } = useSession()
+  const getContent = () => {
+    if (status === "loading")
+      return (
+        <div className={"w-full h-screen flex justify-center items-center"}>
+          <Spinner color={"success"} size={"xl"} />
+        </div>
+      )
 
-  return (
-    <>
-      <p>Hello</p>
-      <code>{JSON.stringify(data)}</code>
-      <button
-        className={
-          "block text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-        }
-        onClick={() => signIn("spotify")}
-      >
-        Sign in
-      </button>
-      <button
-        className={
-          "text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-        }
-        onClick={() => signOut()}
-      >
-        Sign out
-      </button>
-    </>
-  )
+    if (status === "unauthenticated")
+      return (
+        <div className={"w-full h-screen flex flex-col gap-4 justify-center items-center"}>
+          <h1 className={"text-3xl"}>
+            Welcome to <span className={"text-emerald-600"}>Dividify</span>!
+          </h1>
+          <p>Divide your Spotify playlists by collaborators!</p>
+          <Button color={"primary"} onClick={() => signIn("spotify")}>
+            Sign in to Spotify
+          </Button>
+        </div>
+      )
+
+    if (session) {
+      return (
+        <div className={"w-full h-screen flex flex-col gap-4 justify-center items-center"}>
+          <h1 className={"text-3xl"}>
+            Welcome<span className={"text-emerald-600"}> {session.user?.name}</span>!
+          </h1>
+          <Avatar img={session.user?.image ?? ""} rounded size={"lg"} />
+          <Button color={"primary"} onClick={() => signOut()}>
+            Sign out
+          </Button>
+        </div>
+      )
+    }
+  }
+
+  const customTheme: CustomFlowbiteTheme = {
+    button: {
+      color: {
+        primary:
+          "text-white bg-emerald-700 border border-transparent enabled:hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-300 dark:bg-emerald-600 dark:enabled:hover:bg-emerald-700 dark:focus:ring-emerald-800",
+      },
+    },
+    spinner: {
+      color: {
+        success: "fill-emerald-700",
+      },
+    },
+  }
+
+  return <Flowbite theme={{ theme: customTheme }}>{getContent()}</Flowbite>
 }
