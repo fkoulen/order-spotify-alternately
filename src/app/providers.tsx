3cca0880ad7d16b79eb208111f48a3e1be0fe39a
F.S. Koulen <feryllia.koulen@hva.nl>
2023-07-15 17:26:14 +0200

    Implement functionality to reorder playlist

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,33 @@
 
 import { ReactNode } from "react"
 import { SessionProvider } from "next-auth/react"
+import { CustomFlowbiteTheme } from "@/lib/flowbiteComponents"
+import { Flowbite } from "flowbite-react"
 
 type Props = {
   children?: ReactNode
 }
 
 const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>
+  const customTheme: CustomFlowbiteTheme = {
+    button: {
+      color: {
+        primary:
+          "text-white bg-emerald-700 border border-transparent enabled:hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-300 dark:bg-emerald-600 dark:enabled:hover:bg-emerald-700 dark:focus:ring-emerald-800",
+      },
+    },
+    spinner: {
+      color: {
+        success: "fill-emerald-700",
+      },
+    },
+  }
+
+  return (
+    <Flowbite theme={{ theme: customTheme }}>
+      <SessionProvider>{children}</SessionProvider>
+    </Flowbite>
+  )
 }
 
 export default Providers
--- src/lib/spotify/spotifyClient.ts
+++ src/lib/spotify/spotifyClient.ts
@@ -12,6 +12,8 @@ export const setToken = (token: string | undefined) => {
   }
 }
 
+// todo prevent crash on refresh token (right now it crashes because the request fails, is refreshed on refresh)
+
 export type SpotifyClient = typeof import("@/lib/spotify/spotify")
 
 export default spotifyClient
